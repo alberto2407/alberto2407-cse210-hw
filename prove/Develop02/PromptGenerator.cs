8c0dbfbd6b30850c4db99172b653a81889d8c8e2
alberto2407 <122697605+alberto2407@users.noreply.github.com>
2024-09-29 07:55:57 +0200

    Abstraction - Design Activity

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,17 @@
+public class PromptGenerator
+{
+    public string date, promptText, entryText;
+
+    public Entry(string _date, string _promptText, string _entryText)
+    {
+        date = _date;
+        promptText = _promptText;
+        entryText = _entryText;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {date} - Prompt: {promptText}");
+        Console.WriteLine(entryText);
+    }
+}
